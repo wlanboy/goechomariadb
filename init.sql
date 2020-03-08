e078b88b371ab46a9efe61c546980f439edb589a
wlanboy <me@wlanboy.com>
2020-03-08 15:26:01 +0100

    Added init.sql file for database und user creation

---
+++ init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE 'goevent';
+CREATE USER 'goevent' IDENTIFIED BY 'goevent';
+GRANT USAGE ON *.* TO 'goevent'@'%' IDENTIFIED BY 'goevent';
+GRANT ALL privileges ON `goevent`.* TO 'goevent'@'%';
