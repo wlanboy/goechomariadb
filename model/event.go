c1114f4c41e81702bcb8e46f2fc36d214337d9d1
wlanboy <me@wlanboy.com>
2020-03-08 15:54:07 +0100

    Fixed missing uuid support. Moved to string

--- model/event.go
+++ model/event.go
@@ -12,7 +12,7 @@ import (
 type Event struct {
 	//gorm.Model
 	ID        uint64     `gorm:"PRIMARY_KEY;AUTO_INCREMENT;INDEX"`
-	UUID      uuid.UUID  `gorm:"type:uuid;INDEX;NOT NULL" json:"uuid"`
+	UUID      string     `gorm:";INDEX;NOT NULL" json:"uuid"`
 	Name      string     `gorm:"NOT NULL" json:"name"`
 	Type      string     `gorm:"NOT NULL" json:"type"`
 	CreatedAt time.Time  `json:"created"`
@@ -39,24 +39,22 @@ func SaveEvent(event Event, db *gorm.DB) (string, *Event) {
 
 	if error, ok := event.Validate(); !ok {
 		return error, nil
-	} else {
-		uuid, err := uuid.NewV4()
-		if err != nil {
-			fmt.Printf("uuid.NewV4 went wrong: %s", err)
-		} else {
-			event.UUID = uuid
-		}
-		inserterr := db.Create(&event).Error
-		if inserterr != nil {
-			return fmt.Sprintf("Event cannot be saved %s", inserterr), nil
-		} else {
-			return "", &event
-		}
 	}
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		fmt.Printf("uuid.NewV4 went wrong: %s", err)
+	} else {
+		event.UUID = uuid.String()
+	}
+	inserterr := db.Create(&event).Error
+	if inserterr != nil {
+		return fmt.Sprintf("Event cannot be saved %s", inserterr), nil
+	}
+	return "", &event
 }
 
 /*GetEventByID uint*/
-func GetEventByID(id uuid.UUID, db *gorm.DB) (string, *Event) {
+func GetEventByID(id string, db *gorm.DB) (string, *Event) {
 
 	event := Event{}
 	err := db.Where("uuid = ?", id).First(&event).Error
